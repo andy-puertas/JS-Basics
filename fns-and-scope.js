c4c4b2e48a30942ac68c3f33494cab60919feed2
Andy Puertas <andy.puertas08@gmail.com>
2018-02-26 15:35:04 -0700

    done

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,7 +4,14 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+function isTyler(name){
+  if (name === 'Tyler'){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 //////////////////PROBLEM 2////////////////////
 
@@ -12,7 +19,10 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+var getName = () => {
+  var name = prompt('What is your first name?');
+  return name;
+};
 
 
 //////////////////PROBLEM 3////////////////////
@@ -22,9 +32,10 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
-
-
+function welcome(){
+  var newName = getName;
+  alert('Welcome, ' + newName())
+}
 //////////////////PROBLEM 4////////////////////
 
 
@@ -52,17 +63,19 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
-  
+function myName(){
+  return name;
+}
 
+myName('Andy');
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName;
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName);
 
 //////////////////PROBLEM 7////////////////////
 
@@ -70,10 +83,16 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+function outerFn(){
+  return function() {
+    return name;
+  };
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-  //Code Here
+  var innerFn = outerFn()
 
 //Now invoke innerFn.
+
+innerFn
